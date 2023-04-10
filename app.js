2e6368369b141d6656f3011d1c94e84cbed08ebc
Rohan Nayan <rohannayan405@gmail.com>
2023-04-10 21:48:04 +0530

    Setup Node Server

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser);
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/index.html");
+});
 
+app.listen(3000, function(){
+    console.log("Server running on Port 3000");
+})
 
