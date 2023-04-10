ab117c7318e6e1677649e805c29c950cbc3a2f14
Rohan Nayan <rohannayan405@gmail.com>
2023-04-10 22:01:25 +0530

    Added Ejs

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const ejs = require("ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
-    res.sendFile(__dirname+"/index.html");
+    res.render("footer");
 });
 
 app.listen(3000, function(){
